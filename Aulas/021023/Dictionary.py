ecaa4ff53f34ae6f1373fd3a0a3c0d6931bf4018
Lookat Sampaio <lookatsampaio@gmail.com>
2023-10-02 21:39:17 -0300

    acesso a Dictionary e a seus elementos

---
+++ Aulas/021023/Dictionary.py
@@ -0,0 +1,11 @@
+#Para acessar 
+
+lista_valores = [
+    {"id": 1, "nome": "João", "valor": 3},
+    {"id": 2, "nome": "José", "valor": 1},
+    {"id": 3, "nome": "Maria", "valor": 5},
+    {"id": 4, "nome": "Diego", "valor": 2},
+    {"id": 5, "nome": "Aurélio", "valor": 2},
+]
+
+print (lista_valores[0]["nome"])

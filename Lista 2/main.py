ee9df53da4c10dc0bb054c545a9fa876ccea6caf
Lookat Sampaio <lookatsampaio@gmail.com>
2023-10-23 22:34:13 -0300

    terminamos!!

--- Lista 2/main.py
+++ Lista 2/main.py
@@ -7,6 +7,51 @@ produtos = []
 with open("usuario.txt", 'w') as arquivo:
     arquivo.write("julia.meireles,abc123")
 
+column_mapping = {
+    'f': 'id',
+    'g': 'descricao',
+    'h': 'peso',
+    'i': 'valor',
+    'j': 'fornecedor'
+}
+
+def bubble_sort_produtos(produtos, column, reverse=False):
+    n = len(produtos)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if not reverse:
+                if produtos[j][column] > produtos[j + 1][column]:
+                    produtos[j], produtos[j + 1] = produtos[j + 1], produtos[j]
+            else:
+                if produtos[j][column] < produtos[j + 1][column]:
+                    produtos[j], produtos[j + 1] = produtos[j + 1], produtos[j]
+
+def insertion_sort_produtos(produtos, column, reverse=False):
+    for i in range(1, len(produtos)):
+        key = produtos[i]
+        j = i - 1
+        if not reverse:
+            while j >= 0 and key[column] < produtos[j][column]:
+                produtos[j + 1] = produtos[j]
+                j -= 1
+        else:
+            while j >= 0 and key[column] > produtos[j][column]:
+                produtos[j + 1] = produtos[j]
+                j -= 1
+        produtos[j + 1] = key
+
+def selection_sort_produtos_nome(produtos, column, reverse=False):
+    for i in range(len(produtos)):
+        min_idx = i
+        for j in range(i + 1, len(produtos)):
+            if not reverse:
+                if produtos[j][column] < produtos[min_idx][column]:
+                    min_idx = j
+            else:
+                if produtos[j][column] > produtos[min_idx][column]:
+                    min_idx = j
+        produtos[i], produtos[min_idx] = produtos[min_idx], produtos[i]
+
 def Listar_todos_produtos(produtos):
     for produto in produtos:
         print(produto)
@@ -28,54 +73,27 @@ def Listar_produtos_ID(produtos, id):
         if produto['id'] == id:
             print(produto)
 
-def Listar_produtos_ordenados(produtos, ordenacao):
-    if ordenacao == 'A':
-        bubble_sort_produtos(produtos)
-    elif ordenacao == 'Z':
-        bubble_sort_produtos(produtos, reverse=True)
+def Listar_produtos_ordenados(produtos, column, ordenacao):
+    if column not in column_mapping:
+        print("Coluna inválida. Escolha uma coluna válida (f, g, h, i, j).")
+        return
+
+    column_name = column_mapping[column]
+
+    if ordenacao == 'A' or 'a':
+        bubble_sort_produtos(produtos, column_name)
+    elif ordenacao == 'Z 'or 'z':
+        bubble_sort_produtos(produtos, column_name, reverse=True)
     elif ordenacao == 'B':
-        insertion_sort_produtos(produtos)
+        insertion_sort_produtos(produtos, column_name)
     elif ordenacao == 'I':
-        insertion_sort_produtos(produtos)
+        insertion_sort_produtos(produtos, column_name, reverse=True)
     elif ordenacao == 'C':
-        selection_sort_produtos_nome(produtos)
+        selection_sort_produtos_nome(produtos, column_name)
     elif ordenacao == 'S':
-        selection_sort_produtos_nome(produtos)
+        selection_sort_produtos_nome(produtos, column_name, reverse=True)
     else:
         print("Opção de ordenação inválida.")
-
-def bubble_sort_produtos(produtos, reverse=False):
-    n = len(produtos)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if not reverse:
-                if produtos[j]['descricao'] > produtos[j + 1]['descricao']:
-                    produtos[j], produtos[j + 1] = produtos[j + 1], produtos[j]
-            else:
-                if produtos[j]['descricao'] < produtos[j + 1]['descricao']:
-                    produtos[j], produtos[j + 1] = produtos[j + 1], produtos[j]
-
-def insertion_sort_produtos(produtos, reverse=False):
-    for i in range(1, len(produtos)):
-        key = produtos[i]
-        j = i - 1
-        if not reverse:
-            while j >= 0 and key['descricao'] < produtos[j]['descricao']:
-                produtos[j + 1] = produtos[j]
-                j -= 1
-        else:
-            while j >= 0 and key['descricao'] > produtos[j]['descricao']:
-                produtos[j + 1] = produtos[j]
-                j -= 1
-        produtos[j + 1] = key
-
-def selection_sort_produtos_nome(produtos):
-    for i in range(len(produtos)):
-        min_idx = i
-        for j in range(i + 1, len(produtos)):
-            if produtos[j]['descricao'] < produtos[min_idx]['descricao']:
-                min_idx = j
-        produtos[i], produtos[min_idx] = produtos[min_idx], produtos[i]
 
 def editar_produto(produtos, id):
     for produto in produtos:
@@ -102,7 +120,7 @@ def excluir_produto(produtos, id):
             print("Produto com ID {} excluído.".format(id))
             return
 
-    print("Produto com ID {} não encontrado.".format(id) )
+    print("Produto com ID {} não encontrado.".format(id))
 
 def autenticar_usuario():
     usuario = input("Informe o nome de usuário: ")
@@ -117,6 +135,7 @@ def autenticar_usuario():
     else:
         print("Acesso negado")
         return False
+
 def main():
     if not autenticar_usuario():
         print("A autenticação falhou. Encerrando o programa.")
@@ -157,50 +176,29 @@ def main():
             Listar_produtos_ID(produtos, id)
         elif opcao == '3':
             print("=== MENU: Listar todos os produtos ordenados (A/Z) ===")
-            print("METODOS")
-            print("(a) - Bubble sort")
-            print("(b) - Insertion sort")
-            print("(c) - Selection sort")
-            sorting_method = input("Escolha o método (a, b, c): ")
 
-            print("ORDEM")
-            print("(d) - crescente")
-            print("(e) - decrescente")
-            sorting_order = input("Escolha a ordem (d, e): ")
+            # Solicita ao usuário o método de ordenação
+            sorting_method = input("Escolha o método (a - Bubble sort, b - Insertion sort, c - Selection sort): ")
 
+            # Solicita a ordem de ordenação
+            sorting_order = input("Escolha a ordem (A - crescente, Z - decrescente): ")
+
+            # Solicita a coluna para ordenação
             print("COLUNA")
             print("(f) - id")
             print("(g) - descricao")
             print("(h) - peso")
             print("(i) - valor")
             print("(j) - fornecedor")
-            sorting_column = input("Escolha a ordem (f, g, h, i, j): ")
+            sorting_column = input("Escolha a coluna (f, g, h, i, j): ")
 
-            if sorting_method == 'a':
-                bubble_sort_produtos(produtos)
-            elif sorting_method == 'b':
-                insertion_sort_produtos(produtos)
-            elif sorting_method == 'c':
-                selection_sort_produtos_nome(produtos)
+            if sorting_method in ['a', 'b', 'c']:
+                Listar_produtos_ordenados(produtos, sorting_column, sorting_order)
 
-            if sorting_order == 'e':
-                produtos.reverse()
+                # Apresenta a lista de produtos ordenada
+            for produto in produtos:
+                print(f"{column_mapping[sorting_column]}: {produto[column_mapping[sorting_column]]}")
 
-            if sorting_column == 'f':
-                column = 'id'
-            elif sorting_column == 'g':
-                column = 'descricao'
-            elif sorting_column == 'h':
-                column = 'peso'
-            elif sorting_column == 'i':
-                column = 'valor'
-            elif sorting_column == 'j':
-                column = 'fornecedor'
-
-            if column == 'id':
-                Listar_produtos_ID(produtos, column)
-            else:
-                Listar_produtos_ordenados(produtos, column, 'A')
         elif opcao == '5':
             id = int(input("Informe o ID do produto a ser editado: "))
             editar_produto(produtos, id)
@@ -210,7 +208,8 @@ def main():
         elif opcao == '7':
             with open("produtos.txt", "w") as arquivo:
                 for produto in produtos:
-                    arquivo.write("{},{},{},{},{}\n".format(produto['id'], produto['descricao'], produto['peso'], produto['valor'], produto['fornecedor']))
+                    arquivo.write("{},{},{},{},{}\n".format(produto['id'], produto['descricao'], produto['peso'],
+                                                            produto['valor'], produto['fornecedor']))
                 print("Programa encerrado.")
             break
         else:

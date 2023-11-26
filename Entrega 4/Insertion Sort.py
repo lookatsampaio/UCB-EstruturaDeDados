20f0786f59b78ecde5cc4ccfd541519b540c4e48
Lookat Sampaio <lookatsampaio@gmail.com>
2023-11-26 11:41:51 -0300

    contexto de farmacia aplicado

--- Entrega 4/Insertion Sort.py
+++ Entrega 4/Insertion Sort.py
@@ -1,20 +1,14 @@
-def insertionSort(array):
-
-    for step in range(1, len(array)):
-        key = array[step]
-        j = step - 1
-        
-        # Compare key with each element on the left of it until an element smaller than it is found
-        # For descending order, change key<array[j] to key>array[j].        
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
+def insertionSort(medicamento):
+    for etapa in range(1, len(medicamento)):
+        key = medicamento[etapa]
+        j = etapa - 1
+        # Compare key with each element on the left of it until an element smaller than it is found       
+        while j >= 0 and key < medicamento[j]:
+            medicamento[j + 1] = medicamento[j]
             j = j - 1
-        
         # Place key at after the element just smaller than it.
-        array[j + 1] = key
-
-
-data = [9, 5, 1, 4, 3]
-insertionSort(data)
-print('Sorted Array in Ascending Order:')
-print(data)
+        medicamento[j + 1] = key
+Medicamentos = ['Gaballon','Amoxicilina', 'Kaletra', 'Labcaína Geleia 2%', 'Halo']
+print("Lista desorganizada:\n",Medicamentos)
+insertionSort(Medicamentos)
+print("\n\Lista organizada:\n", Medicamentos)

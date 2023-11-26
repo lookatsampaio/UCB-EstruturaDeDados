7da8724662d40824bafd3628f417df547af252a4
Lookat Sampaio <lookatsampaio@gmail.com>
2023-11-25 22:13:30 -0300

    codigo em contexto de farmacia e finalizado

--- Entrega 4/arvore.py
+++ Entrega 4/arvore.py
@@ -3,52 +3,31 @@ class Node:
         self.left = None
         self.right = None
         self.val = item
-
-
-def inorder(root):
-
+def esq_dir(root): #definimos o tipo de leitura para da esquerda para direita (Inorder Tranversal)
     if root:
-        # Traverse left
-        inorder(root.left)
-        # Traverse root
+        esq_dir(root.left)
         print(str(root.val) + "->", end='')
-        # Traverse right
-        inorder(root.right)
-
-
-def postorder(root):
-
+        esq_dir(root.right)
+def sub_top(root): #definimos o tipo de leitura de baixo para o topo (Postorder Tranversal)
     if root:
-        # Traverse left
-        postorder(root.left)
-        # Traverse right
-        postorder(root.right)
-        # Traverse root
+        sub_top(root.left)
+        sub_top(root.right)
         print(str(root.val) + "->", end='')
-
-
-def preorder(root):
-
+def top_sub(root): #definimos o tipo de leitura do topo para baixo (Preorder Tranversal)
     if root:
-        # Traverse root
         print(str(root.val) + "->", end='')
-        # Traverse left
-        preorder(root.left)
-        # Traverse right
-        preorder(root.right)
+        top_sub(root.left)
+        top_sub(root.right)
 
+root = Node('B-Suprin') #definimos o produto como a raiz da arvore
+root.left = Node('Amoxicilina') #definimos o produto como o galho esquerdo de B-Suprin
+root.right = Node('NEOPet') #definimos o produto como o galho direito de B-Suprin
+root.left.left = Node('Halo') #definimos o produto como o galho esquerdo de Amoxilina
+root.left.right = Node('Gaballon') #definimos o produto como o galho direito de Amoxilina
 
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print("Inorder traversal ")
-inorder(root)
-
-print("\nPreorder traversal ")
-preorder(root)
-
-print("\nPostorder traversal ")
-postorder(root)
+print("Da esquerda para direita:\n") #leitura em Inorder Tranversal
+esq_dir(root)
+print("\nDo topo para baixo\n") #leitura em Preorder Tranversal
+top_sub(root)
+print("\nDe baixo para cima\n") #leitura em Postorder Tranversal
+sub_top(root)

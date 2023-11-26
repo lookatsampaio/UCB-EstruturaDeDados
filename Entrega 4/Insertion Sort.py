b8e39ea4b85016d312da455781251e11a5a5da56
Lookat Sampaio <lookatsampaio@gmail.com>
2023-11-26 12:49:43 -0300

    finalizado

--- Entrega 4/Insertion Sort.py
+++ Entrega 4/Insertion Sort.py
@@ -1,14 +1,13 @@
 def insertionSort(medicamento):
     for etapa in range(1, len(medicamento)):
-        key = medicamento[etapa]
-        j = etapa - 1
-        # Compare key with each element on the left of it until an element smaller than it is found       
-        while j >= 0 and key < medicamento[j]:
+        chave = medicamento[etapa]
+        j = etapa - 1   
+        while j >= 0 and chave < medicamento[j]: #compara chave com cada elemento à esquerda até encontrar o menor
             medicamento[j + 1] = medicamento[j]
             j = j - 1
-        # Place key at after the element just smaller than it.
-        medicamento[j + 1] = key
+        # Place chave at after the element just smaller than it.
+        medicamento[j + 1] = chave # localiza a chave depois do elemento menor
 Medicamentos = ['Gaballon','Amoxicilina', 'Kaletra', 'Labcaína Geleia 2%', 'Halo']
-print("Lista desorganizada:\n",Medicamentos)
-insertionSort(Medicamentos)
-print("\n\Lista organizada:\n", Medicamentos)
+print("Lista atual:\n",Medicamentos) #Lista os medicamentos como são inseridos
+insertionSort(Medicamentos) #realiza a organização através do Insertion Sort
+print("\n\Lista organizada:\n", Medicamentos) #Lista os medicamentos já organizados pela insertion sort

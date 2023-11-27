8b02a610086cff5b801fee83ce9768ffe883d3ff
Lookat Sampaio <lookatsampaio@gmail.com>
2023-11-27 19:33:04 -0300

    finalizado

--- Entrega 4/Busca Binária.py
+++ Entrega 4/Busca Binária.py
@@ -1,20 +1,20 @@
-def buscaBinaria(array, x, low, high):
+def buscaBinaria(array, medicamentoProcurado, low, high):
     while low <= high: #repete até que o indicadores maior e o menor se encontrem
         mid = low + (high - low)//2
-        if array[mid] == x:
+        if array[mid] == medicamentoProcurado:
             return mid
-        elif array[mid] < x:
+        elif array[mid] < medicamentoProcurado:
             low = mid + 1
         else:
             high = mid - 1
     return -1
 
 #Código principal
-array = ['B-Suprin',  'Gaballon', 'Halo','Kaletra','Labcaína Geleia 2%','Amoxicilina']
-x = "Gaballon"
-result = buscaBinaria(array, x, 0, len(array)-1)
+array = ['B-Suprin',  'Gaballon', 'Halo','Kaletra','Labcaína Geleia 2%','AmomedicamentoProcuradoicilina'] #lista medicamentos por prateleira
+medicamentoProcurado = "Halo"  #define medicamento a ser procurado
+result = buscaBinaria(array, medicamentoProcurado, 0, len(array)-1)
 if result != -1:
     print("Medicamento na prateleira:  ")
-    print(result+1)
+    print(result+1)                         #define a prateleira em que está o medicamento
 else:
-    print("Medicamento não encontrado")
+    print("Medicamento não encontrado")     #caso não tenha o medicamento encontrado, será informado que não há

3aab91fe81f1e6899440e87b3033d8cad00aa127
Lookat Sampaio <lookatsampaio@gmail.com>
2023-11-27 19:30:43 -0300

    código funcional e farmaceutico

--- Entrega 4/Busca Binária.py
+++ Entrega 4/Busca Binária.py
@@ -9,11 +9,12 @@ def buscaBinaria(array, x, low, high):
             high = mid - 1
     return -1
 
-array = [3, 4, 5, 6, 7, 8, 9]
-x = 6
+#Código principal
+array = ['B-Suprin',  'Gaballon', 'Halo','Kaletra','Labcaína Geleia 2%','Amoxicilina']
+x = "Gaballon"
 result = buscaBinaria(array, x, 0, len(array)-1)
 if result != -1:
-    print("Medicamento na prateleira: ")
+    print("Medicamento na prateleira:  ")
     print(result+1)
 else:
     print("Medicamento não encontrado")
